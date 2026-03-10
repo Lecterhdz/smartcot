a1728eb5e4a0bba2e28aadc64a718775d87abd91
Lecterhdz <dialectycam@gmail.com>
2026-03-10 09:34:32 -0600

    Update reportes.js

--- reportes.js
+++ reportes.js
@@ -249,7 +249,7 @@ window.reportes = {
                     const lineasConcepto = doc.splitTextToSize(descripcion, 170);
                    
                     doc.setFont('helvetica', 'normal');
-                    doc.text('Concepto: ' + descripcion.substring, 20, yPos);
+                    doc.text('Concepto: ' + descripcion.substring(0, 170), 20, yPos);
                     yPos += (lineasConcepto.length * 5);
 
                     doc.text('  Puesto: ' + (mo.puesto || 'Sin puesto'), 140, yPos);
