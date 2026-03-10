581ea6176a55f5714cf3d96db8d71bedab20667e
Lecterhdz <dialectycam@gmail.com>
2026-03-09 22:30:14 -0600

    Update reportes.js

--- reportes.js
+++ reportes.js
@@ -240,13 +240,13 @@ window.reportes = {
                     doc.text(lineasConcepto, 20, yPos);
                     yPos += (lineasConcepto.length * 5);
                     
-                    doc.text('  Puesto: ' + (mo.puesto || 'Sin puesto'), 166, yPos);
+                    doc.text('  Puesto: ' + (mo.puesto || 'Sin puesto'), 140, yPos);
                     yPos += 5;
-                    doc.text('  Jornadas: ' + (mo.jornadas ? mo.jornadas.toFixed(2) : '0') + ' jor', 166, yPos);
+                    doc.text('  Jornadas: ' + (mo.jornadas ? mo.jornadas.toFixed(2) : '0') + ' jor', 140, yPos);
                     yPos += 5;
-                    doc.text('  Costo/Jornada: ' + (mo.costoJornada ? calculator.formatoMoneda(mo.costoJornada) : '$0.00'), 166, yPos);
+                    doc.text('  Costo/Jornada: ' + (mo.costoJornada ? calculator.formatoMoneda(mo.costoJornada) : '$0.00'), 140, yPos);
                     yPos += 5;
-                    doc.text('  Importe: ' + (mo.importe ? calculator.formatoMoneda(mo.importe) : '$0.00'), 166, yPos);
+                    doc.text('  Importe: ' + (mo.importe ? calculator.formatoMoneda(mo.importe) : '$0.00'), 140, yPos);
                     yPos += 8;
                     
                     if (yPos > 250) {
